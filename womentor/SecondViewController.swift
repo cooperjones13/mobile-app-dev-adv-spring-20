0b76954d0a8a735aea843ba4a72d01e3083bbe9b
Cooper Jones <cooper.jones@colorado.edu>
2020-03-23 10:21:34 -0600

    Initial Commit

---
+++ womentor/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  womentor
+//
+//  Created by Cooper Jones on 3/23/20.
+//  Copyright © 2020 Cooper Jones. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ womentorTests/womentorTests.swift
@@ -0,0 +1,34 @@
+//
+//  womentorTests.swift
+//  womentorTests
+//
+//  Created by Cooper Jones on 3/23/20.
+//  Copyright © 2020 Cooper Jones. All rights reserved.
+//
+
+import XCTest
+@testable import womentor
+
+class womentorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
